9668ba32a42a0fcb14b6dafe625ea2c475405ab5
German III <germaniiifelisarta@gmail.com>
2021-07-02 10:07:27 +0800

    Changed codes

--- EMAv1.ino
+++ EMAv1.ino
@@ -1,13 +1,19 @@
 //Packet Variables
-static byte SMP;
+static byte SMP = 0;
 static char SID[4];
 static char RID[4];
 static char ED[44];
 static char HK[10];
 
+//Checking Variables
+static char RID_check[4];
+static char HK_check[10];
+
 //Conditional Variables
 static boolean isReceivingUserContact = false;
 static boolean hasUserContact = false;
+static boolean isReceivingSerialMessages = false;
+static boolean hasSerialMessage = true;
 
 //Counter Variables
 static short SIDCounter = 0;
@@ -17,38 +23,66 @@ void setup(){
 }  
  
 void loop(){
-  char c;
+  char serialData;
   
   if(Serial.available()){
-    c = Serial.read();
-    
+    serialData = Serial.read();
+
+    /* 
+     *  RECEIVE and process Serial Data from User Android Phone on this part 
+     */
+     
     if(isReceivingUserContact)
-      assignSID(c);
-    
+      assignSID(serialData);
+
+    if(isReceivingSerialMessages);  // insert code to pass to SoftwareSerial here
+      
   }else{
-    if(!hasUserContact){
-      // '~' will be used as a control operator
-      Serial.print('~');
-      isReceivingUserContact = true;
-    }else{
-      // for debugging purposes
-      Serial.print("SID ");
+    
+    /* 
+     *  SEND Control Codes on this part of the code
+     *  '1 - 8' can be used as a control operator
+     */
+     
+    if(!hasUserContact){                            // Control Code 1, This will ask for the User SID
+      if(Serial.availableForWrite() >= 63){
+        Serial.write(0x01);
+        isReceivingUserContact = true;
+      }
+      
+    }else if(!hasSerialMessage){                    // Control Code 2, Messages from MessageOut_Table will be sent here
+      if(Serial.availableForWrite() >= 63){
+        Serial.write(0x02)
+        hasSerialMessage = true;
+      }
+                                                    // Control Code 3, This will ask the Android Phone to ready the MessagesIn_Table
+    }else{ 
+      Serial.print("SID : ");
       Serial.print(SID);
     }
     delay(500);  
   }
 }  
 
-
-void assignSID(char c){
+/*------------------------
+ *FUNCTIONS FOR MODULARITY
+  ------------------------*/
+ 
+void assignSID(char serialData){
   while(SIDCounter <= 3){
-    SID[SIDCounter] = c;
+    SID[SIDCounter] = serialData;
     SIDCounter++;
     if(Serial.available())
-      c = Serial.read();
+      serialData = Serial.read();
   }
   if(SIDCounter >= 3){
     hasUserContact = true;
     isReceivingUserContact = false;
+    isReceivingSerialMessages = true;
+    hasSerialMessaage = false;
   }
+}
+
+void hashKeyGenerator(){
+  //insert hash key algorithm here    
 }
